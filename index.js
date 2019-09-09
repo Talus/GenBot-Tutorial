ef722d2b553dd444fb2cf827f28bb59024347db1
Talus <53843843+Talus@users.noreply.github.com>
2019-09-09 16:22:35 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-const token = 'NjIwNDA2MzY1MTQ3NjI3NTIz.XXZzLQ.2L8WjdPdcBSb1FLWmrA7EctfYDs';
+const token = 'NjIwNDA2MzY1MTQ3NjI3NTIz.XXaygw.klPIAqXszSjSVP3vJ6Qwe6riB9U';
+usedCommandRecently4 = new Set();
 
 client.on('ready', () =>{
     console.log('gen bot is now online')
@@ -14,6 +15,13 @@ client.on('message', message =>{
 client.on('message', message =>{
     if (!message.guild) return;
 if (message.content === '=test'){
+    if (usedCommandRecently4.has(message.author.id)){
+        message.channel.send('Cooldown Message')
+    } else{
+        usedCommandRecently4.add(message.author.id);
+        setTimeout(() =>{
+            usedCommandRecently4.delete(message.author.id);
+        }, 10000)
     var string = `test1
     test2
     test3
@@ -22,6 +30,7 @@ if (message.content === '=test'){
     var words = string.split('\n');
     let random = words[Math.floor(Math.random()*words.length)];
     message.author.send(`${random}`);
+};
 };
 });
 client.login(token);
